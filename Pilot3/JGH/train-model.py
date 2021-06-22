89092048a290c78cb7d98558f392a95d3f61186f
jgh9094 <jgh9094@gmail.com>
2021-06-22 12:34:39 -0400

    printing history

--- Pilot3/JGH/train-model.py
+++ Pilot3/JGH/train-model.py
@@ -119,6 +119,7 @@ def GenerateModels(x, y, xT, yT, N, S, V, cfg):
                     epochs=EPOCHS, verbose=2, validation_data=validation_data, callbacks=[stopper]
                   )
 
+  print('hist:', hist)
 
   # iterate through the different ranges for training/testing for each model
   for it in range(N):
