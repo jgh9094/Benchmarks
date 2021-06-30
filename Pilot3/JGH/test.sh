cf1012b56b0e88175d53da611de91dc29ec347b9
jgh9094 <jgh9094@gmail.com>
2021-06-30 15:01:22 -0400

    changed data dir

--- Pilot3/JGH/test.sh
+++ Pilot3/JGH/test.sh
@@ -5,20 +5,20 @@ echo "STARTING THE WRANGLING PROCESS"
 echo "CALLING THE SB FILES NOW..."
 echo ""
 
-echo "RUNNING: single-model.py ../../Data/Pilot3/P3B3_data/ ./ 0 0 0"
-python single-model.py ../../Data/Pilot3/P3B3_data/ ./ 0 0 0
+echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 0 0"
+python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 0 0
 
-echo "RUNNING: single-model.py ../../Data/Pilot3/P3B3_data/ ./ 0 1 1"
-python single-model.py ../../Data/Pilot3/P3B3_data/ ./ 0 1 1
+echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 1 1"
+python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 1 1
 
-echo "RUNNING: single-model.py ../../Data/Pilot3/P3B3_data/ ./ 0 2 2"
-python single-model.py ../../Data/Pilot3/P3B3_data/ ./ 0 2 2
+echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 2 2"
+python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 2 2
 
-echo "RUNNING: single-model.py ../../Data/Pilot3/P3B3_data/ ./ 0 3 3"
-python single-model.py ../../Data/Pilot3/P3B3_data/ ./ 0 3 3
+echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 3 3"
+python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 3 3
 
-echo "RUNNING: single-model.py ../../Data/Pilot3/P3B3_data/ ./ 0 4 4"
-python single-model.py ../../Data/Pilot3/P3B3_data/ ./ 0 4 4
+echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 4 4"
+python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 4 4
 
 echo ""
 echo "======================"
