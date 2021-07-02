2242a81dcb4e1be414338ce20741b8b17423de24
jgh9094 <jgh9094@gmail.com>
2021-07-01 23:45:02 -0400

    all in one go

--- Pilot3/JGH/test.sh
+++ Pilot3/JGH/test.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
 # Start the process
-echo "STARTING THE WRANGLING PROCESS"
-echo "CALLING THE SB FILES NOW..."
+echo "STARTING PROCESS"
 echo ""
 
-echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 0 0"
-python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 0 0
+echo "CREATING SINGLE MODELS FOR TASK 1"
 
-echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 1 1"
-python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 1 1
+echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 0 0 0"
+python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 0 0 0
 
-echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 2 2"
-python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 2 2
+echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 1 1 0"
+python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 1 1 0
 
-echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 3 3"
-python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 3 3
+echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 2 2 0"
+python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 2 2 0
 
-echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 4 4"
-python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 4 4
+echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 3 3 0"
+python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 3 3 0
+
+echo "RUNNING: single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 4 4 0"
+python single-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 4 4 0
+
+echo "CREATING ENSEMBLE FOR TASK 1"
 
 echo "RUNNING: ensemble-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ ./ 0 5 0"
 python ensemble-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ ./ 0 5 0
 
+echo "DISTILLING KNOWLEDGE INTO SINGLE CNN"
+
 echo "RUNNING: python distiller.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ ./ 0 0"
 python distiller.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ ./ 0 0
 
+echo "CREATING MTCNN"
+
+echo "RUNNING: python mtcnn-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 0"
+python mtcnn-model.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ ./ 0 0
+
+echo "DISTILLING MTCNN INTO SMALLER MTCNN"
+
+echo "RUNNING: python distiller-mtcnn.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ /home/60h/GEM-SUMMER-2021/Benchmarks/Pilot3/JGH/MTModel-0/ ./  0 0 7"
+python distiller-mtcnn.py /home/60h/GEM-SUMMER-2021/Benchmarks/Data/P3B3_data/ /home/60h/GEM-SUMMER-2021/Benchmarks/Pilot3/JGH/MTModel-0/ ./  0 0 7
 echo ""
 echo "======================"
 echo "FINISHED RUNNING FILES"
