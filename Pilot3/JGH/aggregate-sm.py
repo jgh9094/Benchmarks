cb2c32f92150533e543ecdb419c649a3048949fb
jgh9094 <jgh9094@gmail.com>
2021-07-09 18:20:56 -0400

    testing

--- Pilot3/JGH/aggregate-sm.py
+++ Pilot3/JGH/aggregate-sm.py
@@ -39,7 +39,7 @@ def GetDataDirs(dir,p):
   return dirs
 
 # will look through all dirs and average out their data (testing, training, validate)
-def AverageData(dirs,task,seed,dump,data):
+def AverageData(dirs,task,dump,data):
   # get training data
   print('AVERAGING',data.upper(),'DATA...')
 
@@ -111,13 +111,13 @@ def main():
   dirs = GetDataDirs(args.data_dir,args.proportion)
 
   # Step 2: Average training data
-  AverageData(dirs,task,seed,args.dump_dir, 'training')
+  AverageData(dirs,task,args.dump_dir, 'training')
 
   # Step 3: Average testing data
-  AverageData(dirs,task,seed,args.dump_dir, 'testing')
+  AverageData(dirs,task,args.dump_dir, 'testing')
 
   # Step 3: Average testing data
-  AverageData(dirs,task,seed,args.dump_dir, 'validating')
+  AverageData(dirs,task,args.dump_dir, 'validating')
 
 
 if __name__ == '__main__':
