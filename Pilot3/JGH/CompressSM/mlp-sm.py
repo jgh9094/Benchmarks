8f678fcc5676939a6b32ac9c1613549f1c6bcc75
jgh9094 <jgh9094@gmail.com>
2021-07-14 16:07:54 -0400

    debugging

--- Pilot3/JGH/CompressSM/mlp-sm.py
+++ Pilot3/JGH/CompressSM/mlp-sm.py
@@ -22,13 +22,13 @@ from keras.regularizers import l2
 from keras.callbacks import EarlyStopping
 from keras.utils import to_categorical
 
-# OLCF imports
-from mpi4py import MPI
+# # OLCF imports
+# from mpi4py import MPI
 
-# global variables
-COMM = MPI.COMM_WORLD
-RANK = COMM.Get_rank()
-SIZE = COMM.size #Node count. size-1 = max rank.
+# # global variables
+# COMM = MPI.COMM_WORLD
+# RANK = COMM.Get_rank()
+# SIZE = COMM.size #Node count. size-1 = max rank.
 EPOCHS = 100
 YLAB = '/gpfs/alpine/world-shared/med106/yoonh/storageFolder/HardLabels/'
 CLASS =  [4,639,7,70,326]
@@ -139,7 +139,7 @@ def main():
   parser.add_argument('offset',       type=int,      help='Seed offset for rng')
 
   # RANK is synonomous with the task task being evaluated
-  # RANK = 0 # used for example right now
+  RANK = 0 # used for example right now
   task = int(RANK)
   print('task:', task,flush=True)
 
