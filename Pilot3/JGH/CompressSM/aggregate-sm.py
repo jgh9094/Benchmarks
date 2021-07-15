8d670a9bff96b3710f25e07bac553db56add8a79
jgh9094 <jgh9094@gmail.com>
2021-07-14 22:59:11 -0400

    debugging

--- Pilot3/JGH/CompressSM/aggregate-sm.py
+++ Pilot3/JGH/CompressSM/aggregate-sm.py
@@ -76,6 +76,7 @@ def AverageData(dirs,task,dump,data):
 
     # iteratate through each file and update the matrix
     for i in range(X.shape[0]):
+      print(i)
       for j in range(X.shape[1]):
         mat[i][j] += X[i][j]
 
