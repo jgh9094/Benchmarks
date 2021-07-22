b78a3ed6c1d0a449415028b847e6d53fde6d873e
jgh9094 <jgh9094@gmail.com>
2021-07-22 17:07:15 -0400

    debugging

--- Pilot3/JGH/Data/Code/gather.py
+++ Pilot3/JGH/Data/Code/gather.py
@@ -36,10 +36,12 @@ def main():
   parser.add_argument('data_dir',     type=str,      help='Where is the data?')
   parser.add_argument('dump_dir',     type=str,      help='Where are we dumping the output?')
   parser.add_argument('model',        type=int,      help='What type of models are we getting')
+  parser.add_argument('models',       type=int,      help='What type of models are we getting')
   parser.add_argument('name',         type=str,      help='Name of file to output')
 
   # parse all the argument
   args = parser.parse_args()
+  print(args)
 
   # what are the inputs
   print('data_dir:', args.data_dir, flush= True)
