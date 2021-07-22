1a45ca7301f79952100a95ff265854d156123833
jgh9094 <jgh9094@gmail.com>
2021-07-22 16:23:42 -0400

    initial push

---
+++ Pilot3/JGH/Data/Code/gather.py
@@ -0,0 +1,39 @@
+'''
+Created by: Jose Guadalupe Hernandez
+Email: jgh9094@gmail.com
+
+export PATH=$HOME/anaconda3/bin:$PATH
+
+Python file will aggregate
+'''
+
+# general python imports
+import numpy as np
+import argparse
+import pickle as pk
+import psutil
+import os
+import pandas as pd
+
+from sklearn.metrics import f1_score
+
+
+def main():
+  # generate and get arguments
+  parser = argparse.ArgumentParser(description='Process arguments for model training.')
+  parser.add_argument('data_dir',     type=str,      help='Where are we dumping the output?')
+
+  # parse all the argument
+  args = parser.parse_args()
+
+  # what are the inputs
+  print('data_dir:', args.data_dir, flush= True)
+
+  df = pd.read_csv(args.data_dir + 'MTModel-0_Rank-0/MicMacTest_R0.csv')
+
+  print(df)
+
+
+
+if __name__ == '__main__':
+  main()
