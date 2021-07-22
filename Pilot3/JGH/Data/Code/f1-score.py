e9d2bd7031c96089761c0b66eb2c2a3a318385b9
jgh9094 <jgh9094@gmail.com>
2021-07-22 13:05:24 -0400

    debugging

--- Pilot3/JGH/Data/Code/f1-score.py
+++ Pilot3/JGH/Data/Code/f1-score.py
@@ -32,13 +32,19 @@ def main():
   # what are the inputs
   print('data_dir:', args.data_dir, flush= True)
 
+  pred = []
+
   # get the softmax values of the our predictions from raw logits
   predT = np.load(args.data_dir + 'testing-task-0.npy')
   for i in range(len(predT)):
     print(predT[i])
     predT[i] = softmax(predT[i])
     print(predT[i])
-    break
+    m = np.argmax(predT[i])
+    print(m)
+    pred.append(m)
+
+  print(pred)
 
 
 if __name__ == '__main__':
