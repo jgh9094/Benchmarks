fbbfb300c6876682bc5620a41b6dae70f6b84e30
jgh9094 <jgh9094@gmail.com>
2021-07-22 16:45:22 -0400

    initial push

--- Pilot3/JGH/Data/Code/gather.py
+++ Pilot3/JGH/Data/Code/gather.py
@@ -38,11 +38,11 @@ def main():
 
   df = pd.read_csv(args.data_dir + 'MTModel-0_Rank-0/MicMacTest_R0.csv', index_col=False)
 
-  x = df.iloc[1].to_list()
+  x = df.iloc[1].to_list().pop(0)
   x.pop(0)
 
   print(df)
-  print('x:', x)
+  print('x:', df.iloc[1].to_list().pop(0))
   print(data)
 
 
